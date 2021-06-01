c74f0535f360332b8684916bfbd722238b2042a8
boris-couderc <boris-couderc@users.noreply.github.com>
2021-06-02 01:51:53 +0200

    menu js v1

--- public/wp-content/themes/mstrategie/src/scripts/modules/Navbar.js
+++ public/wp-content/themes/mstrategie/src/scripts/modules/Navbar.js
@@ -1,13 +1,36 @@
 export default class Navbar {
     constructor() {
 
+        this.nav = document.querySelector('.js-menu');
+        this.toggle = document.querySelector('.js-menu-mobile-toggle');
+
+        this.navIsOpen = false;
+
+
         this.init()
 
     }
 
     init() {
-
         console.log("init Navbar");
+
+        this.toggle.addEventListener('click', this.toggleMenu.bind(this))
+
+
+    }
+
+    toggleMenu() {
+
+        console.log('toggleMenu')
+
+        if(this.navIsOpen) {
+            this.navIsOpen = false;
+            this.nav.classList.remove('--open');
+
+        } else {
+            this.navIsOpen = true;
+            this.nav.classList.add('--open');
+        }
 
     }
 }
